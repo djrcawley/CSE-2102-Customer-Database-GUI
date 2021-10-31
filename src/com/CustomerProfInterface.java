2d599049a06cef5fef5f814164f2e0a262ecb32e
djrcawley <djrcawley@yahoo.com>
2021-10-31 18:55:46 +0000

    Comments

--- src/com/CustomerProfInterface.java
+++ src/com/CustomerProfInterface.java
@@ -4,13 +4,14 @@ import java.util.*;
 import java.lang.*;
 
 public class CustomerProfInterface {
-    private CustomerProfDB databaseFile;
+    private CustomerProfDB databaseFile; //Database
 
-    CustomerProfInterface(String file){
-        File fileName = new File(file);
-        databaseFile = new CustomerProfDB(file);
+    public CustomerProfInterface(String file){
+        File fileName = new File(file); //File Object
+        databaseFile = new CustomerProfDB(file); //Initialize Database
     }
     public void getUserChoice(){
+        //Print Options to User
         System.out.println("Please enter the number of the task you would like");
         System.out.println("1. Find Customer");
         System.out.println("2. Update Customer");
@@ -18,73 +19,80 @@ public class CustomerProfInterface {
         System.out.println("4. Remove Customer");
         System.out.println("5. Display All Customers");
         System.out.println("Exit");
-
+        //Read option and Save
         Scanner in = new Scanner(System.in);
         String selection = in.nextLine();
-
+        //Continue until user enters exit
         while (!selection.equals("exit") && !selection.equals("Exit")){
             switch (selection) {
-                case "1" -> findCustomerProf();
-                case "2" -> updateCustomerProf();
+                case "1" -> findCustomerProf(); //Call findCustomerProf
+                case "2" -> updateCustomerProf(); // Call updateCustomerProf
                 case "3" -> {
                     CustomerProf newCustomer = createNewCustomerProf();
                     databaseFile.insertNewProfile(newCustomer);
+                    //Create new customer and insert into database
                 }
-                case "4" -> deleteCustomerProf();
-                case "5" -> displayAllCustomerProf();
+                case "4" -> deleteCustomerProf(); //Delete Customer
+                case "5" -> displayAllCustomerProf(); //Display Customers with Similar AdminId
                 default -> System.out.println("Invalid Operation. Please use a number (1-3) or enter 'exit' to close the session");
             }
             System.out.println("Enter new command or 'Exit'");
+            //Read new input from user
             selection = in.nextLine();
         }
+        //User has exited; Save to file
         writeToDB();
+        //Let user know program has terminated
         System.out.println("Successfully closed session");
     }
 
     public void deleteCustomerProf(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Once a profile is deleted it cannot be recovered");
+        Scanner in = new Scanner(System.in); //Create Scanner
+        System.out.println("Once a profile is deleted it cannot be recovered"); //Warning to user
         System.out.println("Enter AdminID:");
-        String adminID = in.nextLine();
+        String adminID = in.nextLine(); //Get adminID
         System.out.println("Enter LastName:");
-        String lastName = in.nextLine();
-        boolean wasDeleted = databaseFile.deleteProfile(adminID, lastName);
+        String lastName = in.nextLine(); //Get lastName
+        boolean wasDeleted = databaseFile.deleteProfile(adminID, lastName); //Try to delete
         if(wasDeleted){
-            System.out.println("Account was successfully deleted");
+            System.out.println("Account was successfully deleted"); //Succesfull deletion
         }
         else{
-            System.out.println("Account was not deleted. Account does not exist or incorrect adminID");
+            System.out.println("Account was not deleted. Account does not exist or incorrect adminID"); //Account not found
         }
     }
 
     public void findCustomerProf(){
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); //Create Scanner
         System.out.println("Enter AdminID:");
-        String adminID = in.nextLine();
+        String adminID = in.nextLine(); //Get adminID
         System.out.println("Enter LastName:");
-        String lastName = in.nextLine();
-        CustomerProf customer = databaseFile.findProfile(adminID, lastName);
-        if (customer == null){
+        String lastName = in.nextLine(); //Get lastName
+        CustomerProf customer = databaseFile.findProfile(adminID, lastName); //Try to find profile
+        if (customer == null){ //Profile not found
             String adminIdString = String.format("%s has not created this customer.", adminID);
             System.out.println(adminIdString);
         }
         else{
-            displayCustomerProf(customer);
+            displayCustomerProf(customer); //Display found customer profile
         }
     }
 
     public void updateCustomerProf(){
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); //Create Scanner
         System.out.println("Enter AdminID:");
-        String adminID = in.nextLine();
+        String adminID = in.nextLine(); //Get adminID
         System.out.println("Enter LastName:");
-        String lastName = in.nextLine();
-        CustomerProf customer = databaseFile.findProfile(adminID, lastName);
+        String lastName = in.nextLine(); //Get lastName
+        CustomerProf customer = databaseFile.findProfile(adminID, lastName); //Try to find profile
         if (customer == null){
+            //Cusomter not found
             String adminIdString = String.format("%s has not created this customer.", adminID);
-            System.out.println(adminIdString);
+            System.out.println(adminIdString); //Let user know customer was not found
         }
         else{
+            //Customer Found
+            //Prompt user with options
             System.out.println("Customer Found. Please indicate which item you would like to edit.");
             System.out.println("1. Change Address");
             System.out.println("2. Change Phone");
@@ -95,148 +103,162 @@ public class CustomerProfInterface {
             System.out.println("7. Vehicle Type");
             System.out.println("8. Vehicle Method");
             System.out.println("Exit");
-            String selection = in.nextLine();
+            String selection = in.nextLine(); //Read option
 
+            //Prompt user until 'exit' is entered
             while (!selection.equals("exit") && !selection.equals("Exit")){
                 switch (selection) {
                     case "1" -> {
+                        //Option 1: Update Address
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.updateAddress(value);
+                        String value = in.nextLine(); //Read new address
+                        customer.updateAddress(value); //Call Update
                     }
                     case "2" -> {
+                        //Option2: Update phone
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.updatePhone(value);
+                        String value = in.nextLine(); //Read new phone
+                        customer.updatePhone(value); //Call updatePhone
                     }
                     case "3" -> {
+                        //Option3: Update Use
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.updateUse(value);
+                        String value = in.nextLine(); //Read new use
+                        customer.updateUse(value); //Call updateUse
                     }
                     case "4" -> {
+                        //Option4: Update Status
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.updateStatus(value);
+                        String value = in.nextLine(); //Read new status
+                        customer.updateStatus(value); //Call updateStatus
                     }
                     case "5" -> {
+                        //Option 5: Update Vehicle Model
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.getVehicleInfo().updateModel(value);
+                        String value = in.nextLine(); //Read new model
+                        customer.getVehicleInfo().updateModel(value); //Call updateModel
                     }
                     case "6" -> {
+                        //Option 6: Update Vehicle Year
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.getVehicleInfo().updateYear(value);
+                        String value = in.nextLine(); //Read new year
+                        customer.getVehicleInfo().updateYear(value); //Call updateYear
                     }
                     case "7" -> {
+                        //Option7: Update Vehicle Type
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.getVehicleInfo().updateType(value);
+                        String value = in.nextLine(); //Read new type
+                        customer.getVehicleInfo().updateType(value); //Call updateType
                     }
                     case "8" -> {
+                        //Option 8: Update Vehicle Method
                         System.out.println("Please Enter New Value");
-                        String value = in.nextLine();
-                        customer.getVehicleInfo().updateMethod(value);
+                        String value = in.nextLine(); //Read new method
+                        customer.getVehicleInfo().updateMethod(value); //Call updateMethod
                     }
+                    //User enters unavailable option
                     default -> System.out.println("Invalid Operation");
                 }
+                //Prompt user to enter another operation
                 System.out.println("To edit another value enter its respective number or 'Exit'");
-                selection = in.nextLine();
+                selection = in.nextLine(); // read option
             }
 
         }
     }
     public void displayCustomerProf(CustomerProf customer){
+        //Format profiles as they are displayed in the database file
         String profile = String.format("%s, %s, %s, %s, %s, %.2f, %s, %s, %s, %s, %s, %s",
                 customer.getadminID(), customer.getFirstName(), customer.getLastName(),
                 customer.getAddress(), customer.getPhone(), customer.getIncome(),
                 customer.getStatus(), customer.getUse(), customer.getVehicleInfo().getModel(),
                 customer.getVehicleInfo().getYear(), customer.getVehicleInfo().getType(),
                 customer.getVehicleInfo().getMethod());
-        System.out.println(profile);
+        System.out.println(profile); //Print profile
     }
 
     public void displayAllCustomerProf(){
-        boolean userFound = false;
-        Scanner in = new Scanner(System.in);
+        boolean userFound = false; //Was a user found
+        Scanner in = new Scanner(System.in); //Create Scanner
         System.out.println("Enter AdminID:");
-        String adminID = in.nextLine();
+        String adminID = in.nextLine(); //Get adminID
 
-        CustomerProf customer = databaseFile.findFirstProfile();
+        CustomerProf customer = databaseFile.findFirstProfile(); //Get first profile
         if (customer == null){
-            System.out.println("There are no users in the database.");
+            System.out.println("There are no customers in the database."); //There are no customers
         }
         else{
             while(customer != null){
-                if (customer.getadminID().equals(adminID)){
-                    displayCustomerProf(customer);
-                    userFound = true;
+                if (customer.getadminID().equals(adminID)){ //Check customers with given adminID
+                    displayCustomerProf(customer); //Display found profile
+                    userFound = true; //A user was found
                 }
-                customer = databaseFile.findNextProfile();
+                customer = databaseFile.findNextProfile(); //Find next customer in database
             }
 
         }
 
         if(!userFound){
+            //No customers were found with the given adminID
             String adminIdString = String.format("%s has not created any users.", adminID);
             System.out.println(adminIdString);
         }
     }
 
     public void writeToDB(){
-        databaseFile.writeAllCustomerProf();
+        databaseFile.writeAllCustomerProf(); //Write all profiles to database file
     }
 
     CustomerProf createNewCustomerProf(){
-        Scanner in = new Scanner(System.in);
-        boolean valid_float = false;
+        Scanner in = new Scanner(System.in); //Create Scanner
+        boolean valid_float = false; //isValidFloat
 
         System.out.println("Enter AdminID:");
-        String adminID = in.nextLine();
+        String adminID = in.nextLine(); //Get adminID
         System.out.println("Enter FirstName:");
-        String firstName = in.nextLine();
+        String firstName = in.nextLine(); //Get firstName
         System.out.println("Enter LastName:");
-        String lastName = in.nextLine();
+        String lastName = in.nextLine(); //Get lastName
         System.out.println("Enter Address:");
-        String address = in.nextLine();
+        String address = in.nextLine(); //Get address
         System.out.println("Enter PhoneNumber:");
-        String phone = in.nextLine();
+        String phone = in.nextLine(); //Get phone
         System.out.println("Enter Income:");
         float income = 0F;
-        while(!valid_float){
+        while(!valid_float){ //Read until user enters valid float
             try{
-                income = Float.parseFloat(in.nextLine());
-                valid_float = true;
+                income = Float.parseFloat(in.nextLine()); //Convert to float
+                valid_float = true; //Valid float found
             }catch(Exception e){
-                System.out.println("Please input Income as a float:");
+                System.out.println("Please input Income as a float:"); //Prompt user to enter float
             }
         }
         System.out.println("Enter Status:");
-        String status = in.nextLine();
+        String status = in.nextLine(); //Get Status
         System.out.println("Enter Use:");
-        String use = in.nextLine();
+        String use = in.nextLine(); //Get Use
 
-        VehicleInfo vehicleInfo = createNewVehicleInfo();
+        VehicleInfo vehicleInfo = createNewVehicleInfo(); //Create vehicle
+        //Return new CusomterProf object
         return new CustomerProf(adminID, firstName, lastName, address, phone, income, status, use, vehicleInfo);
     }
 
     VehicleInfo createNewVehicleInfo(){
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); //Create Scanner
         System.out.println("Enter CarModel:");
-        String model = in.nextLine();
+        String model = in.nextLine(); //Get model
         System.out.println("Enter Year:");
-        String year = in.nextLine();
+        String year = in.nextLine(); //Get year
         System.out.println("Enter Type:");
-        String carType = in.nextLine();
+        String carType = in.nextLine(); //Get Type
         System.out.println("Enter Method:");
-        String method = in.nextLine();
+        String method = in.nextLine(); //Get Method
 
-        return new VehicleInfo(model, year, carType, method);
+        return new VehicleInfo(model, year, carType, method); //Create and return vehicle
     }
 
     public static void main(String[] args) {
-        CustomerProfInterface inter = new CustomerProfInterface("test_data.txt");
-        inter.getUserChoice();
+        CustomerProfInterface inter = new CustomerProfInterface("test1.txt"); //Create interface object with a fileName
+        inter.getUserChoice(); //Call getUserChoice to begin
     }
 }
--- src/com/VehicleInfo.java
+++ src/com/VehicleInfo.java
@@ -1,11 +1,13 @@
 package com;
 
 public class VehicleInfo {
+    //Private Vehicle Details
     private String model;
     private String year;
     private String type;
     private String method;
 
+    //Vehicle Constructor
     VehicleInfo(String carModel, String carYear, String carType, String carMethod){
         model = carModel;
         year = carYear;
@@ -14,34 +16,46 @@ public class VehicleInfo {
     }
 
     public String getModel(){
-        return model;
+        return model; //Get model
     }
 
     public String getYear(){
-        return year;
+        return year; //Get year
     }
 
     public String getType(){
-        return type;
+        return type; //Get type
     }
 
     public String getMethod(){
-        return method;
+        return method; //Get method
     }
 
     public void updateModel(String newModel){
-        this.model = newModel;
+        this.model = newModel; //Set model
     }
 
     public void updateYear(String newYear){
-        this.year = newYear;
+        this.year = newYear; //Set year
     }
 
     public void updateType(String newType){
-        this.type = newType;
+        this.type = newType; //Set type
     }
 
     public void updateMethod(String newMethod){
-        this.method = newMethod;
+        this.method = newMethod; //Set method
+    }
+
+    public static void main(String[] args) {
+        VehicleInfo vehicle1 = new VehicleInfo("Honda Civic", "2021", "Hatchback", "New");
+        vehicle1.updateModel("Toyota Forerunner");
+        vehicle1.updateYear("2018");
+        vehicle1.updateType("SUV");
+        vehicle1.updateMethod("Used");
+        System.out.println(vehicle1.getModel());
+        System.out.println(vehicle1.getYear());
+        System.out.println(vehicle1.getType());
+        System.out.println(vehicle1.getMethod());
     }
 }
